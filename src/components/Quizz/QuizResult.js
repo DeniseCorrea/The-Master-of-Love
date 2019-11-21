666524347ac1842d702ab6278039c17067bc32b5
ines <inesqtr@gmail.com>
2019-11-21 16:46:33 +0000

    fixed compile

--- src/components/Quizz/QuizResult.js
+++ src/components/Quizz/QuizResult.js
@@ -1,10 +1,11 @@
-import react from 'react';
+import React from 'react';
+import { Link } from "react-router-dom";
 
 
 const QuizResult = ({ currentScore, character }) => {
     const { name, homeworld } = character;
 
-    handleFinalResult = (currentScore) => {
+    const handleFinalResult = (currentScore) => {
         if (currentScore >= 50) {
             return (
                 <div>
