f0f69287546d117062e3d98572722afdd01e20fb
ines <inesqtr@gmail.com>
2019-11-22 13:06:31 +0000

    fixed details

--- src/components/Match/Match.js
+++ src/components/Match/Match.js
@@ -7,32 +7,43 @@ function Match({ history, character }) {
   return (
     <>
 
-<div className="container">
-    <div className="header">
-    <div className="third2"><h1>
-    The Master
+      <div className="container">
+        <div className="header">
+          <div className="third2"><h1>
+            The Master
     </h1></div>
 
-    <div className="third"><Link to="/preferences">
-      <img src="./logo.png" alt="logo"/>></Link>
-    </div>
+          <div className="third"><Link to="/preferences">
+            <img src="./logo.png" alt="logo" />></Link>
+          </div>
 
-    <div className="third2"> <h1>
-    Of Love
+          <div className="third2"> <h1>
+            Of Love
     </h1></div>
-  </div>
-
-     <div className="content">
-        <div className="textbox">
-          <h2>It's a match!</h2><br></br>
-          <h2>You And <b><i>{name}</i></b> Like Eachother, But...</h2><br></br>
-          <h2>Patience You Must Have, My Young Padawan.</h2><br></br>
-          <h2>First The Test You Must Pass!</h2><br></br>
         </div>
-    </div>
-    <div className="footer">
-        <button className="button" onClick={() => history.push("/quiz")}>Test Me!</button>
-      </div>
+
+        <div className="content">
+          <div className="textbox">
+            <div className="give-padding">
+              <h2>It's a match!</h2>
+            </div>
+
+            <div className="give-padding">
+              <h2>You And <b><i>{name}</i></b> Like Eachother, But...</h2>
+            </div>
+
+            <div className="give-padding">
+              <h2>Patience You Must Have, My Young Padawan.</h2>
+            </div>
+
+            <div className="give-padding">
+              <h2>First The Test You Must Pass!</h2>
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          <button className="button" onClick={() => history.push("/quiz")}>Test Me!</button>
+        </div>
       </div>
     </>
   );
